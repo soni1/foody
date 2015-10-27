bc94a8b152ffdeb692df801760274929a593a927
Waqar Ashraf <WaqarAshraf222@gmail.com>
2015-10-27 12:52:04 +0200

    fixes ui to upload image in add_recipes

--- client/templates/recipes/recipes.js
+++ client/templates/recipes/recipes.js
@@ -2,5 +2,9 @@ Template.recipes.helpers({
     'showRecipes':function(){
         return Recipes.find();
 
+    },
+    'images': function () {
+        return RecipesImages.findOne({_id:id}) ;
     }
+
 })
