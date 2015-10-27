d22fbaf80cf9a39ea64416399dfaa79d9ec663c9
Waqar Ashraf <WaqarAshraf222@gmail.com>
2015-10-28 00:48:21 +0200

    showing images

--- client/templates/recipes/recipes.js
+++ client/templates/recipes/recipes.js
@@ -1,10 +1,8 @@
 Template.recipes.helpers({
     'showRecipes':function(){
         return Recipes.find();
-
     },
     'images': function () {
-        return RecipesImages.findOne({_id:id}) ;
+        return RecipesImages.findOne() ;
     }
-
 })
