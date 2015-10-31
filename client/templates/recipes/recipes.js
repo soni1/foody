a92300976664fb6278232a78cc4c79cb17e01ad1
Waqar Ashraf <WaqarAshraf222@gmail.com>
2015-10-31 22:45:48 +0200

    adds like recipe feature

--- client/templates/recipes/recipes.js
+++ client/templates/recipes/recipes.js
@@ -5,4 +5,13 @@ Template.recipes.helpers({
     'images': function () {
         return RecipesImages.findOne({_id:this.image}) ;
     }
+});
+
+Template.recipes.events({
+    "click [data-action='addLikes']":function(event){
+        event.preventDefault();
+        currentRecipe = Recipes.findOne(this._id);
+         Recipes.update({ _id:this._id}, {$set:{likes: +1}  });
+
+    }
 })
