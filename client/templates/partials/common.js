1cc3aae5f490d651c60571d386dfae8b7d41cd7d
Waqar Ashraf <WaqarAshraf222@gmail.com>
2015-11-15 13:53:01 +0200

    adds helper to short the description text of recipe

--- client/templates/partials/common.js
+++ client/templates/partials/common.js
@@ -1,18 +1,5 @@
-// Check For Empty Fields
-isNotEmpty = function(value) {
-    if (value && value !== ''){
-        return true;
-    }
-    FlashMessages.sendError('Please fill all fields',{ autoHide: true, hideDelay: 3000 });
-    return false;
-};
-
-// Validate Email
-isEmail = function(value) {
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (filter.test(value)) {
-        return true;
-    }
-    FlashMessages.sendError('Please use a valid email address',{ autoHide: true, hideDelay: 3000 });
-    return false;
-};
+Template.registerHelper('shortText', function(text, length){
+    var newText = text.substring(0, length);
+    newText = newText.substr(0, Math.min(newText.length, newText.lastIndexOf(" ")))
+    return new Spacebars.SafeString(newText)
+});
