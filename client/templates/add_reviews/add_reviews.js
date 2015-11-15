50e2f07dad319628d05fd6a73008ccb6f3a56381
Waqar Ashraf <WaqarAshraf222@gmail.com>
2015-11-15 13:03:55 +0200

    adds rating and review feature

--- client/templates/add_reviews/add_reviews.js
+++ client/templates/add_reviews/add_reviews.js
@@ -1,1 +1,19 @@
- 
+Template.add_review.events({
+    'submit .add-review':function(event){
+        var rating = event.target.rating.value;
+        var review = event.target.review.value;
+        if(review!=""){
+            Reviews.insert({
+                rating:rating,
+                review:review,
+                recipeId:Router.current().data()._id
+            });
+            FlashMessages.sendSuccess('Review Added',{ autoHide: true, hideDelay: 3000 });
+            Router.go('/');
+        }
+        else{
+            FlashMessages.sendError('Review field is empty',{ autoHide: true, hideDelay: 3000 });
+        }
+        return false;
+    }
+})
---
+++ client/templates/reviews/reviews.js
@@ -0,0 +1,19 @@
+Template.reviews.helpers({
+    'showReviews': function () {
+        return Reviews.find({recipeId: Router.current().data()._id})
+    },
+
+    countReviews: function(){
+        return Reviews.find({recipeId: Router.current().data()._id}).count();
+    },
+    averageRating: function() {
+        var reviews = Reviews.find({recipeId: Router.current().data()._id}).fetch();
+        if(reviews.length===0)
+        return 0;
+        var ratings = _.pluck(reviews, 'rating');
+        var ratingsInt = ratings.map( function( num ){ return parseInt( num, 10 ) } );
+        var sum = ratingsInt.reduce(function(pv, cv){return pv + cv;},0);
+        var avg = sum / ratings.length;
+        return Math.round(avg);
+    }
+});
