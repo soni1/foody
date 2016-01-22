9470fe34170a9cf9f48ed302ab843a794c7d75bf
Waqar Ashraf <WaqarAshraf222@gmail.com>
2016-01-22 07:53:16 +0200

    fixes the like button

--- client/templates/recipes/recipes.js
+++ client/templates/recipes/recipes.js
@@ -1,18 +1,22 @@
 Template.recipes.helpers({
-    'showRecipes':function(){
+    'showRecipes': function () {
         return Recipes.find();
     },
     'images': function () {
-        return RecipesImages.findOne({_id:this.image}) ;
+        return RecipesImages.findOne({_id: this.image});
     }
 });
 
 Template.recipes.events({
-    "click [data-action='addLikes']":function(event){
+    "click [data-action='addLikes']": function (event) {
         event.preventDefault();
-        if (_.contains(Recipes.voters,Meteor.userId() ))
-            FlashMessages.sendError("You already liked this recipe", { hideDelay: 1000 });
-              Recipes.update(this._id,
-                  {$addToSet:{voters:Meteor.userId()}, $inc:{likes: 1}  });
+        var recipe = Recipes.findOne({_id: this._id});
+        if (recipe) {
+            if (_.contains(recipe.voters, Meteor.userId())) {
+                FlashMessages.sendError("You already liked this recipe", {hideDelay: 1000});
+                return false;
+            }
+            Recipes.update(this._id, {$addToSet: {voters: Meteor.userId()}, $inc: {likes: 1}});
+        }
     }
 });
